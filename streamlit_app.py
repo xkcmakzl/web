a50e8fba5c7436ff287d8230d501b7a792295bc0
xkcmakzl <H098s02112@chunchon.gwe.hs.kr>
2025-11-10 12:32:03 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,14 +5,28 @@ import pandas as pd
 
 # --- 1. 페이지 기본 설정 ---
 st.set_page_config(
-    page_title="Streamlit 마법 교실",
+    page_title="장우의 첫번쨰 페이지",
     page_icon="🔮",
     layout="wide"
 )
 
 # --- 2. 페이지 타이틀 ---
 st.title("Streamlit 마법 교실 🔮")
-st.subheader("HTML/CSS를 활용해 멋진 효과를 만들어 봐요!")
+st.subheader("춘천고등학교든 레포지토리(저장소)에 streamlit_app.py 만들고 다음 내용 복붙해서 집어넣기
+
+import streamlit as st
+import pandas as pd
+
+# --- 1. 페이지 기본 설정 ---
+st.set_page_config(
+    page_title="장우의 첫번쨰 페이지",
+    page_icon="🔮",
+    layout="wide"
+)
+
+# --- 2. 페이지 타이틀 ---
+st.title("Streamlit 마법 교실 🔮")
+st.subheader("춘천고등학교")
 st.markdown("---") # 구분선
 
 # --- 3. 모든 커스텀 CSS ---
@@ -196,7 +210,7 @@ with col1:
 
 with col2:
     st.write("유튜브 동영상 예제")
-    st.video("https://www.youtube.com/watch?v=B2iAodr0fOo")
+    st.video("https://www.youtube.com/watch?v=qrqHlgqNTHo")
 
 # --- 10. 마무리 ---
 st.markdown("---")
